41a079f5554cfc7faa408ca0694a383473f96ece
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-08 18:26:12 +0700

    refactor: update API request logic to dynamically construct base URL and improve endpoint handling

--- lib/api.ts
+++ lib/api.ts
@@ -13,9 +13,24 @@ export interface ApiError extends BaseResponse {
     data?: never
 }
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_PREFIX = '/api-proxy';
+const getBaseUrl = () => {
+    if (typeof window !== 'undefined') return '';
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    return 'http://localhost:3000'; // Server Local
+};
 async function request<T>(endpoint: string, options: RequestInit = {}): Promise<ApiResponse<T>> {
+    let fullUrl = '';
+    const baseUrl = getBaseUrl();
+    if (endpoint.startsWith('http')) {
+        fullUrl = endpoint;
+    } else {
+        const path = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
+        const finalPath = path.startsWith(API_PREFIX) ? path : `${API_PREFIX}${path}`;
+
+        fullUrl = `${baseUrl}${finalPath}`;
+    }
     const option: RequestInit = {
         ...options,
         credentials: 'include', // Include cookies for cross-origin requests
@@ -24,7 +39,7 @@ async function request<T>(endpoint: string, options: RequestInit = {}): Promise<
             ...options.headers,
         },
     };
-    const response = await fetch(`${BASE_URL}${endpoint}`, option);
+    const response = await fetch(fullUrl, option);
     const data = await response.json();
     if (!response.ok) {
         throw Error((data as ApiError).error);
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+    console.log('🔗 Rewriting to API:', apiUrl);
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: `${apiUrl}/:path*`,
+      },
+    ];
+  }
+}
 
-export default withNextIntl(nextConfig);
+  export default withNextIntl(nextConfig);
