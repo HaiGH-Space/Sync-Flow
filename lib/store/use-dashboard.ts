dafec6851ae0f100141e285a4108d1b3e1ee9b2a
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-10 21:23:47 +0700

    feat: enhance dashboard store with project management actions and initial state

--- lib/store/use-dashboard.ts
+++ lib/store/use-dashboard.ts
@@ -1,12 +1,35 @@
+import { create } from "zustand"
 import { Project } from "../services/project"
 
 type DashboardState = {
     isOpenLeftSidebar: boolean,
-    project: Project
+    projectIdSelected: string | null,
+    projects: Project[]
+}
+
+const initialState: DashboardState = {
+    isOpenLeftSidebar: true,
+    projectIdSelected: null,
+    projects: [],
 }
 
 type DashboardActions = {
     toggleLeftSidebar: () => void,
+    setProjectIdSelected: (projectId: string | null) => void,
+    setProjects: (projects: Project[]) => void,
+    updateProject: (project: Project) => void,
+    reset: () => void
 }
 
 type DashboardStore = DashboardState & DashboardActions
+
+export const useDashboardStore = create<DashboardStore>((set, get) => ({
+    ...initialState,
+    toggleLeftSidebar: () => set((state) => ({ isOpenLeftSidebar: !state.isOpenLeftSidebar })),
+    setProjectIdSelected: (projectId) => set({ projectIdSelected: projectId }),
+    setProjects: (projects: Project[]) => set({ projects }),
+    updateProject: (project: Project) => set((state) => ({
+        projects: state.projects.map((p) => p.id === project.id ? project : p)
+    })),
+    reset: () => set(initialState)
+}))
