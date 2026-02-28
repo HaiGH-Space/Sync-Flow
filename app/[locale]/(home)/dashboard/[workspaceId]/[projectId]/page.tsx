a690c4d4e573a714cc0a9d66760c6e6260c971c5
HaiGH-Space <nthai0932169055@gmail.com>
2026-02-28 20:25:27 +0700

    feat: add BoardCanvas component and implement dashboard page logic for task management

--- app/[locale]/(home)/dashboard/[workspaceId]/[projectId]/page.tsx
+++ app/[locale]/(home)/dashboard/[workspaceId]/[projectId]/page.tsx
@@ -1,7 +1,11 @@
 'use client'
+import BoardCanvas from "@/components/canvas/BoardCanvas"
+import { NavigateType, useDashboard } from "@/lib/store/use-dashboard"
 import { useParams } from "next/navigation"
 
 export default function DashBoardPage() {
     const { projectId } : { workspaceId: string, projectId: string } = useParams()
-    
+    const navigateActive = useDashboard((state) => (state.activeNavigate))
+
+    return navigateActive.value === NavigateType.BOARD ? <BoardCanvas /> : <div>Other View</div>
 }
